1de7d4665c11832b5786144610cb205f76bdd2cf
Neha Gupta <adityamusics123@gmail.com>
2024-07-14 12:29:11 +0530

    edited

--- Day2/Patternprint.java
+++ Day2/Patternprint.java
@@ -3,11 +3,12 @@ package Day2;
 public class Patternprint {
     public static void main(String[] args) {
         int n=5;
-        for (int i=n;i<=1;i++) {
-            for (int k=1;k<=n-i;k++)
-                System.out.print("  ");
-            for(int j=1;j<=i;j++)
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++)
+                if(i+j== n+1 || i==j )
                 System.out.print("* ");
+                else
+            System.out.print("  ");
             System.out.println();
         }
     }

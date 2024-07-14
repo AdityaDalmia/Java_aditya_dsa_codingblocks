5fb0ee273cdcf6cc889f9a8ca81d67b39f8d7ccf
Neha Gupta <adityamusics123@gmail.com>
2024-07-14 09:49:05 +0530

    re update

---
+++ Day1/update.java
@@ -0,0 +1,12 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class update {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            System.out.println("value input by user is " + a);
+        }
+    }
+}

96fcfff86c1319356bf1bc8ba3e50020fd089340
Neha Gupta <adityamusics123@gmail.com>
2024-08-03 12:14:18 +0530

    final commit

---
+++ Day7/algo.java
@@ -0,0 +1,21 @@
+package Day7;
+
+public class algo {
+    static int[] NGER(int[] arr){
+        int maxElement=0;
+        int[] NGER = new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+            NGER[i]=maxElement;
+            maxElement=Math.max(maxElement,arr[i]);
+            }
+            return NGER;
+        }
+
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 5};
+            int[] result = NGER(arr);
+            for (int i = 0; i < result.length; i++) {
+                System.out.print(result[i] + " ");
+                }
+        }
+    }

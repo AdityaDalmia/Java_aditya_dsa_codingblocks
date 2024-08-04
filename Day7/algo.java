a712ae63dbc6d7e503dd5a825c5003703651b8a0
Neha Gupta <adityamusics123@gmail.com>
2024-08-04 10:18:30 +0530

    Final commit

--- Day7/algo.java
+++ Day7/algo.java
@@ -12,7 +12,7 @@ public class algo {
         }
 
         public static void main(String[] args) {
-            int[] arr = {1, 2, 3, 4, 5};
+            int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
             int[] result = NGER(arr);
             for (int i = 0; i < result.length; i++) {
                 System.out.print(result[i] + " ");
---
+++ Day8/tc.java
@@ -0,0 +1,9 @@
+package Day8;
+
+public class tc {
+    public static void main(String[] args) {
+        int N=50;
+        for(int i=0;i<N;i++)
+        System.out.println("Stripe Club");
+    }
+}
